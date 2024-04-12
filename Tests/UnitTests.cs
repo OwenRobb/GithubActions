88144b669ea0e9c8ffbbbeff0ae7a51fcf3fb1c6
Owen Robb <orobb2@huskers.unl.edu>
2024-04-11 19:36:07 -0500

    Power test 3 is wrong

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -31,5 +31,100 @@ namespace GithubActionsLab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_Valid_Robb()
+        {
+            Assert.AreEqual(3, Program.Add("4", "1"));
+            Assert.AreEqual(6, Program.Add("12", "6"));
+            Assert.AreEqual(1, Program.Add("1", "0"));
+        }
+
+        [Test]
+        public void Subtract_Invalid_Robb()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_Null_Robb()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
+
+        [Test]
+        public void Multiply_Valid_Robb()
+        {
+            Assert.AreEqual(3, Program.Add("3", "1"));
+            Assert.AreEqual(6, Program.Add("3", "2"));
+            Assert.AreEqual(1, Program.Add("1", "1"));
+        }
+
+        [Test]
+        public void Multiply_Invalid_Robb()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_Null_Robb()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
+
+                [Test]
+        public void Divide_Valid_Robb()
+        {
+            Assert.AreEqual(3, Program.Add("6", "2"));
+            Assert.AreEqual(6, Program.Add("12", "2"));
+            Assert.AreEqual(10, Program.Add("20", "2"));
+        }
+
+        [Test]
+        public void Divide_Invalid_Robb()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Divide_Null_Robb()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
+
+        [Test]
+        public void Power_Valid_Robb()
+        {
+            Assert.AreEqual(4, Program.Add("2", "2"));
+            Assert.AreEqual(16, Program.Add("4", "2"));
+            Assert.AreEqual(24, Program.Add("5", "2"));
+        }
+
+        [Test]
+        public void Power_Invalid_Robb()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Power_Null_Robb()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
     }
 }
